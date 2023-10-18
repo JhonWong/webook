0ee538170ce8b930004c9f1287f51ef68b54bd1d
JhonWong <1290773033@qq.com>
2023-10-18 22:43:05 +0800

    添加mongo测试代码

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/stretchr/testify v1.8.3
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.737
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms v1.0.737
+	go.mongodb.org/mongo-driver v1.9.0
 	go.uber.org/atomic v1.9.0
 	go.uber.org/mock v0.2.0
 	go.uber.org/zap v1.21.0
@@ -29,15 +30,24 @@ require (
 )
 
 require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
 
---
+++ mongo/crud_test.go
@@ -0,0 +1,70 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/event"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"testing"
+	"time"
+)
+
+func TestMongo(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	monitor := &event.CommandMonitor{
+		Started: func(ctx context.Context, startedEvent *event.CommandStartedEvent) {
+			//fmt.Println(startedEvent.Command)
+		},
+	}
+
+	opts := options.Client().ApplyURI("mongodb://root:example@localhost:27017").
+		SetMonitor(monitor)
+	client, err := mongo.Connect(ctx, opts)
+	assert.NoError(t, err)
+
+	mdb := client.Database("webook")
+	col := mdb.Collection("articles")
+	defer func() {
+		_, err = col.DeleteMany(ctx, bson.M{})
+	}()
+
+	// 新增数据
+	res, err := col.InsertOne(ctx, Article{
+		Id:      123,
+		Tittle:  "my tittle",
+		Content: "my content",
+	})
+	assert.NoError(t, err)
+	fmt.Printf("id: %s \n", res.InsertedID)
+
+	// 更新数据
+	filter := bson.M{"id": 123}
+	update := bson.M{"$set": bson.M{"tittle": "new tittle"}}
+	upres, err := col.UpdateOne(ctx, filter, update)
+	assert.NoError(t, err)
+	fmt.Println("affected", upres.ModifiedCount)
+
+	upres, err = col.UpdateMany(ctx, filter, bson.M{"$set": Article{Tittle: "2333"}})
+	assert.NoError(t, err)
+
+	// 查找数据
+	var art Article
+	err = col.FindOne(ctx, filter).Decode(&art)
+	assert.NoError(t, err)
+	fmt.Printf("%#v \n", art)
+}
+
+type Article struct {
+	Id       int64  `bson:"id,omitempty"`
+	Tittle   string `bson:"tittle,omitempty"`
+	Content  string `bson:"content,omitempty"`
+	AuthorId int64  `bson:"authorId,omitempty"`
+	Status   uint8  `bson:"status,omitempty"`
+	Ctime    int64  `bson:"ctime,omitempty"`
+	Utime    int64  `bson:"utime,omitempty"`
+}
